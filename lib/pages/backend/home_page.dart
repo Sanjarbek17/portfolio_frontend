4e029ed22237a06400788605e4be60532bcdd379
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-01 19:05:34 +0500

    home page is done

--- lib/pages/backend/home_page.dart
+++ lib/pages/backend/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:portfolio_frontend/pages/backend/providers/page_index_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -29,10 +30,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: background,
-      body: Padding(
-        padding: const EdgeInsets.only(top: 32.0),
-        child: Consumer<PageIndexProvider>(builder: (context, value, child) => pages[value.pageIndex]),
-      ),
+      body: Consumer<PageIndexProvider>(builder: (context, value, child) => pages[value.pageIndex]),
     );
   }
 }
@@ -46,34 +44,70 @@ class MainPage extends StatelessWidget {
       children: [
         const NavBar(currentPageIndex: 0),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1),
-          child: const Row(
+          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1, vertical: 60),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(
-                width: 537,
-                child: Text.rich(
-                  TextSpan(
-                    children: [
+              Expanded(
+                flex: 50,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text.rich(
                       TextSpan(
-                        text: 'Sanjarbek is a ',
-                        style: TextStyle(color: Colors.white, fontSize: 32, fontFamily: 'Fira Code', fontWeight: FontWeight.w600),
+                        children: [
+                          TextSpan(
+                            text: 'Sanjarbek is a ',
+                            style: TextStyle(color: Colors.white, fontSize: 32, fontFamily: 'Fira Code', fontWeight: FontWeight.w600),
+                          ),
+                          TextSpan(
+                            text: 'back-end developer',
+                            style: TextStyle(color: Color(0xFFC778DD), fontSize: 32, fontFamily: 'Fira Code', fontWeight: FontWeight.w600),
+                          ),
+                          TextSpan(
+                            text: ' and ',
+                            style: TextStyle(color: Colors.white, fontSize: 32, fontFamily: 'Fira Code', fontWeight: FontWeight.w600),
+                          ),
+                          TextSpan(
+                            text: 'front-end developer',
+                            style: TextStyle(color: Color(0xFFC778DD), fontSize: 32, fontFamily: 'Fira Code', fontWeight: FontWeight.w600),
+                          ),
+                        ],
                       ),
-                      TextSpan(
-                        text: 'back-end developer',
-                        style: TextStyle(color: Color(0xFFC778DD), fontSize: 32, fontFamily: 'Fira Code', fontWeight: FontWeight.w600),
+                    ),
+                    const SizedBox(height: 20),
+                    Text('He crafts responsive websites where technologies meet creativity', style: Theme.of(context).textTheme.titleSmall),
+                    const SizedBox(height: 20),
+                    InkWell(
+                      onTap: () => context.read<PageIndexProvider>().setPageIndex(2),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                        decoration: BoxDecoration(border: Border.all(color: primary)),
+                        child: Text('Contact me !!', style: Theme.of(context).textTheme.titleSmall!.copyWith(color: white, fontWeight: FontWeight.w500)),
                       ),
-                      TextSpan(
-                        text: ' and ',
-                        style: TextStyle(color: Colors.white, fontSize: 32, fontFamily: 'Fira Code', fontWeight: FontWeight.w600),
-                      ),
-                      TextSpan(
-                        text: 'front-end developer',
-                        style: TextStyle(color: Color(0xFFC778DD), fontSize: 32, fontFamily: 'Fira Code', fontWeight: FontWeight.w600),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              )
+              ),
+              Expanded(
+                flex: 50,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Positioned(
+                      top: 70,
+                      left: 50,
+                      child: SvgPicture.asset('assets/icons/developer.svg'),
+                    ),
+                    Image.asset('assets/images/avatar.png'),
+                    Positioned(
+                      bottom: 50,
+                      right: 50,
+                      child: SvgPicture.asset('assets/icons/Dots.svg'),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
--- lib/pages/backend/widgets/nav_bar_widget.dart
+++ lib/pages/backend/widgets/nav_bar_widget.dart
@@ -14,33 +14,61 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.8,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SvgPicture.asset('assets/icons/logo.svg', color: white),
-              const SizedBox(width: 8),
-              Text(
-                'Sanjarbek',
-                style: Theme.of(context).textTheme.titleSmall!.copyWith(color: white, fontWeight: FontWeight.w600),
-              ),
-            ],
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(
+            top: 0,
+            left: -1 * MediaQuery.of(context).size.width * 0.07,
+            child: Column(
+              children: [
+                // VerticalDivider(color: white, thickness: 10, width: 10),
+                Container(
+                  width: 1,
+                  height: 190,
+                  color: gray,
+                ),
+                // FIXME: buttons now pressing because of the stack
+                const SizedBox(height: 8),
+                const InkWell(child: Icon(Icons.discord, color: gray, size: 17)),
+                const SizedBox(height: 8),
+                const InkWell(child: Icon(Icons.telegram, color: gray, size: 17)),
+                const SizedBox(height: 8),
+                const InkWell(child: Icon(Icons.mail_outline, color: gray, size: 17)),
+              ],
+            )),
+        Padding(
+          padding: const EdgeInsets.only(top: 32),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SvgPicture.asset('assets/icons/logo.svg', color: white),
+                    const SizedBox(width: 8),
+                    Text(
+                      'Sanjarbek',
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(color: white, fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    navbarButton(context, 'Home', selected: 0),
+                    navbarButton(context, 'Projects', selected: 1),
+                    navbarButton(context, 'About Me', selected: 2),
+                    navbarButton(context, 'Contacts', selected: 3),
+                  ],
+                ),
+              ],
+            ),
           ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              navbarButton(context, 'Home', selected: 0),
-              navbarButton(context, 'Projects', selected: 1),
-              navbarButton(context, 'About Me', selected: 2),
-              navbarButton(context, 'Contacts', selected: 3),
-            ],
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
