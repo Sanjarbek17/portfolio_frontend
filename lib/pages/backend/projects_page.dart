462431187f522ecb874d1f447f53baa3be0feaff
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-02 09:56:54 +0500

    projects page is done

--- lib/pages/backend/projects_page.dart
+++ lib/pages/backend/projects_page.dart
@@ -17,7 +17,7 @@ class ProjectPage extends StatelessWidget {
               Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  navbarButton(context, 'Projects'),
+                  navbarButton(context, 'projects'),
                   const SizedBox(width: 16),
                   Container(width: MediaQuery.of(context).size.width * 0.4, height: 1, color: primary),
                 ],
@@ -32,25 +32,47 @@ class ProjectPage extends StatelessWidget {
           Expanded(
             child: GridView.builder(
               shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 16, crossAxisSpacing: 16),
-              itemCount: 3,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+                childAspectRatio: 0.81,
+              ),
+              itemCount: 2,
               itemBuilder: (context, index) {
                 return Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.transparent,
+                  // width: 100,
+                  // height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border.all(color: white),
+                  ),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(child: Image.asset('assets/images/avatar.png')),
+                      Expanded(child: Image.asset('assets/images/Rectangle 22.png')),
                       const Divider(thickness: 1, color: white),
                       Expanded(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text('Project', style: Theme.of(context).textTheme.titleMedium!.copyWith(color: white)),
-                            const SizedBox(height: 8),
-                            Text('Description', style: Theme.of(context).textTheme.titleSmall!.copyWith(color: white)),
-                          ],
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            // mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text('project', style: Theme.of(context).textTheme.titleMedium!.copyWith(color: white)),
+                              const SizedBox(height: 8),
+                              SizedBox(width: 200, child: Text('Minecrsting ' * 100, maxLines: 1, overflow: TextOverflow.ellipsis, style: Theme.of(context).textTheme.titleSmall)),
+                              Expanded(
+                                child: ListView(
+                                  scrollDirection: Axis.horizontal,
+                                  children: [
+                                    linkButton(context, 'Live <~~>'),
+                                    linkButton(context, 'Github >='),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ],
@@ -60,6 +82,25 @@ class ProjectPage extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Padding linkButton(BuildContext context, String text) {
+    // TODO: Make this a button
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: Align(
+        alignment: Alignment.center,
+        child: SizedBox(
+          width: 110,
+          height: 37,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(border: Border.all(color: primary)),
+            child: Text(text, style: Theme.of(context).textTheme.titleSmall!.copyWith(color: white)),
+          ),
+        ),
       ),
     );
   }
