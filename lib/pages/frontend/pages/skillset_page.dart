47d3dede255551655e1e5651a7aca6e978460fb1
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-14 13:05:00 +0500

    skillset page is done

---
+++ lib/pages/frontend/pages/skillset_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/skill_set_card.dart';
+
+class SkillSetPage extends StatelessWidget {
+  const SkillSetPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 72.0, vertical: 100),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Expanded(
+              flex: 4,
+              child: SkillSetCard(
+                title: 'Skillset',
+                description:
+                    'With skills in over 4 different fields of design, I am the perfect person to hire when it comes to a full fledged project. Whatever your needs are, I can pretty much take on any challenge.',
+                author: 'Sanjarbek S.',
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Center(
+                child: GridView.count(
+                  physics: const NeverScrollableScrollPhysics(),
+                  crossAxisCount: 2,
+                  // crossAxisSpacing: 0.001,
+                  // mainAxisSpacing: 0.1,
+                  childAspectRatio: 1.5,
+                  children: const [
+                    SkillSetCard(
+                      icon: Icon(Icons.code, color: Colors.blue, size: 55),
+                      title: 'Product Design',
+                      description: 'Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.',
+                    ),
+                    SkillSetCard(
+                      icon: Icon(Icons.code, color: Colors.blue, size: 55),
+                      title: 'Product Design',
+                      description: 'Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.',
+                    ),
+                    SkillSetCard(
+                      icon: Icon(Icons.code, color: Colors.blue, size: 55),
+                      title: 'Product Design',
+                      description: 'Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.',
+                    ),
+                    SkillSetCard(
+                      icon: Icon(Icons.code, color: Colors.blue, size: 55),
+                      title: 'Product Design',
+                      description: 'Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/frontend/widgets/skill_set_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SkillSetCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final String? author;
+  final Icon? icon;
+
+  const SkillSetCard({
+    super.key,
+    required this.title,
+    required this.description,
+    this.author,
+    this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        icon ?? const SizedBox(),
+        Text(title, style: TextStyle(color: Colors.white, fontSize: icon == null ? 55 : 24, fontFamily: 'Sen', fontWeight: FontWeight.w700)),
+        const SizedBox(height: 20),
+        SizedBox(
+          width: 370,
+          child: Text(description, style: TextStyle(color: const Color(0xFF8491A0), fontSize: icon == null ? 18 : 16, fontFamily: 'Sen', fontWeight: FontWeight.w400)),
+        ),
+        const SizedBox(height: 20),
+        Visibility(
+          visible: author != null,
+          child: Text(author ?? '', style: const TextStyle(color: Colors.white, fontSize: 18, fontFamily: 'Sen', fontWeight: FontWeight.w400)),
+        ),
+      ],
+    );
+  }
+}
