26d490ab1931b13fd1bfdba942506569c5005890
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-14 15:46:47 +0500

    some changes

--- lib/pages/frontend/pages/contact_page.dart
+++ lib/pages/frontend/pages/contact_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/message_input.dart';
+
 class ContactPage extends StatelessWidget {
   const ContactPage({super.key});
 
@@ -68,40 +70,6 @@ class ContactPage extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class MesageInput extends StatelessWidget {
-  final String name;
-  final int maxLines;
-  final TextEditingController? controller;
-
-  const MesageInput({
-    super.key,
-    required this.name,
-    this.maxLines = 1,
-    this.controller,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(name, style: const TextStyle(color: Colors.white, fontSize: 16, fontFamily: 'Sen', fontWeight: FontWeight.w400)),
-        const SizedBox(height: 10),
-        TextField(
-          controller: controller,
-          maxLines: maxLines,
-          decoration: const InputDecoration(
-            fillColor: Color(0xFF181823),
-            filled: true,
-            border: OutlineInputBorder(borderSide: BorderSide.none),
-            enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
-          ),
-        ),
-      ],
     );
   }
 }
