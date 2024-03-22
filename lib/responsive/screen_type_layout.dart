bb2089ab223db5ba74fb65e226c2da73b5f83b38
Sanjarbek5656 <sanjarbeksaidov1717@gmail.com>
2024-03-23 00:07:09 +0300

    responsive file added

---
+++ lib/responsive/screen_type_layout.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_frontend/enums/device_screen_type.dart';
+import 'package:portfolio_frontend/responsive/responsive_builder.dart';
+
+class ScreenTypeLayout extends StatelessWidget {
+  // Mobile will be returned by default
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget? desktop;
+
+  const ScreenTypeLayout({
+    Key? key,
+    required this.mobile,
+    this.tablet,
+    this.desktop,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        // If sizing indicates Tablet and we have a tablet widget then return
+        if (sizingInformation.deviceScreenType == DeviceScreenType.Tablet) {
+          if (tablet != null) {
+            return tablet as Widget;
+          }
+        }
+
+        // If sizing indicates desktop and we have a desktop widget then return
+        if (sizingInformation.deviceScreenType == DeviceScreenType.Desktop) {
+          if (desktop != null) {
+            return desktop as Widget;
+          }
+        }
+
+        // Return mobile layout if nothing else is supplied
+        return mobile;
+      },
+    );
+  }
+}
